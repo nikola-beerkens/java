fd4c9031d8aeec2f50f23398e6e0f377b64e62ca
Nikola Beerkens <122792637+nikola-beerkens@users.noreply.github.com>
2026-09-11 16:12:13 +0200

    implemented feedback

--- assignment-hangman/app/src/hangman/GallowsUI.java
+++ assignment-hangman/app/src/hangman/GallowsUI.java
@@ -4,32 +4,29 @@ import java.util.Scanner;
 
 public class GallowsUI {
 
-    private Scanner scanner;
-    private String readString;
-    private Gallows game;
+    private static final Scanner scanner = new Scanner(System.in);
+    private final Gallows game;
 
     public GallowsUI(){
-        this.scanner = new Scanner(System.in);
+        System.out.println("Welcome to Hangman!");
+        System.out.println("Please enter a word or press Enter to randomly pick one");
+        String readString = scanner.nextLine();
+        if(readString.isEmpty()){
+            game = new Gallows();
+            System.out.println("Picking random word");
+        }
+        else{
+            game = new Gallows(readString);
+        }
     }
 
     public void startGame(){
-		System.out.println("Welcome to Hangman!");
-		System.out.println("Please enter a word or press Enter to randomly pick one");
-		readString = scanner.nextLine();
-		if(readString.isEmpty()){
-			 game = new Gallows();
-             System.out.println("Picking random word");
-		}
-		else{
-			 game = new Gallows(readString);
-		}
 		System.out.println("Remaining mistakes: " + game.getLives());
 		System.out.println("Guessed letters: " + game.toString());
         System.out.println("Word: " + game.print().toString());
-        while(game.win()){
+        do{
             char c = scanner.next().charAt(0);
-		    game.guessLetter(c);
-            if (game.getWord().indexOf(c) != -1){
+            if (game.guessLetter(c)){
                 System.out.println(c + " is in the word!");
             }
             else{
@@ -38,6 +35,6 @@ public class GallowsUI {
             System.out.println("Remaining mistakes: " + game.getLives());
 		    System.out.println("Guessed letters: " + game.toString());
             System.out.println("Word: " + game.print().toString());
-        }
+        }while(game.win());
     }
 }
